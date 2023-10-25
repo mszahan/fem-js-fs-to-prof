dfabb95de5eb7905e07855a9187bc29c9cbe0a70
mszahan <mszahan1@gmail.com>
2023-10-25 22:11:30 +0600

    used ternary operator for conditionals

---
+++ conditional_loop/ternary_conditional.js
@@ -0,0 +1,12 @@
+x = false
+if (x){
+    console.lor('It is true');
+
+}else{
+    console.log('It is false');
+}
+
+// implementing the same thing with ternary operator
+// x ? runs_if_true : runs_if_false
+
+x ? console.log('It is true') : console.log('It is false');
