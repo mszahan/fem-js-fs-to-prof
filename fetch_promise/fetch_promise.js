74b7886f5967a6693a0e3358b601edbd6d52bd5b
mszahan <mszahan1@gmail.com>
2023-10-26 17:57:49 +0600

    explored the fetch await and promise in js

---
+++ fetch_promise/fetch_promise.js
@@ -0,0 +1,23 @@
+// it returns promise with pending state
+// const dog = fetch('https://dog.ceo/api/breed/hound/list');
+// console.log(dog)
+
+
+
+// working on browser bring some response but not the actual data
+//...... but showing sytax error in terminal
+// let response = await fetch('https://dog.ceo/api/breed/hound/list');
+// console.log(response.json())
+
+
+
+// to avoid the sytax error you have to use await in async function
+async function dog(){
+    // it returns pending promise so used await to make it fulfiled
+    let response = await fetch('https://dog.ceo/api/breed/hound/list');
+    // this also returns peding promise to make it work you need await
+    body = await response.json()
+    console.log(body)
+}
+
+dog();
