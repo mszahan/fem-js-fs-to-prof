23f9f57b22a22e298bc763cf08910cea8019694b
mszahan <mszahan1@gmail.com>
2023-10-28 11:01:25 +0600

    used modules import export and added package.json

---
+++ modules/myModules.js
@@ -0,0 +1,6 @@
+function addTwoNum(a, b){
+    return a + b;
+}
+
+
+export {addTwoNum};
---
+++ modules/playGround.js
@@ -0,0 +1,5 @@
+import { addTwoNum } from "./myModules.js";
+
+// to import and export you need package.json file.....
+// ..... with type module, otherwise it will throw systax error
+console.log(addTwoNum(25, 35));
